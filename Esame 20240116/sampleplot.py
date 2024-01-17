76b9421407b04c89285c7249fa571baa9647dc47
Antonio Solida <solida.antonio@gmail.com>
2024-01-17 10:55:45 +0100

    Last one commit

--- Esame 20240116/sampleplot.py
+++ Esame 20240116/sampleplot.py
@@ -8,16 +8,19 @@ import matplotlib as mpl
 import matplotlib.colors as mc
 from plots import plot_line, set_fonts
 
+# USAGE: python sampleplot.py sample.data
+
 mu=10
 lam=10
 ts=0.1
 delta=0.1
 def theoretical(x):
-    return x*1.05+0.11
+    # return x*1.05+0.11
+    return (1-x)*(delta+1/mu) + x*(1/(mu - lam*x))
     
 
 if __name__ == "__main__":
-    set_fonts()
+    # set_fonts()  # NON USARE SE NON SI HA LATEX INSTALLATO
     # plot respone time
     fig, ax = plt.subplots()
     ax.set(xlabel='$x$', ylabel='Time [s]')
